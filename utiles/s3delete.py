c5560be9865f2fc0162d2e6961edb44f40bbea0f
Deepthi <pachigulladeepthi@gmail.com>
2024-06-13 20:33:59 +0530

    Added empty and delete s3bucket using python boto3 sdk

---
+++ utiles/s3delete.py
@@ -0,0 +1,26 @@
+import boto3
+
+def empty_bucket(bucket_name):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    
+    # Delete all objects
+    #print(f"Deleting all objects from bucket: {bucket_name}")
+    #bucket.objects.all().delete()
+    
+    versioning = s3.BucketVersioning(bucket_name)
+    if versioning.status == 'Enabled':
+        print(f"Deleting all versions from bucket: {bucket_name}")
+        bucket.object_versions.all().delete()
+
+def delete_bucket(bucket_name):
+    s3 = boto3.client('s3')
+    empty_bucket(bucket_name)
+    s3.delete_bucket(Bucket=bucket_name)
+    print(f"Bucket {bucket_name} deleted")
+
+
+if __name__ == '__main__':
+    bucket_name = 'newbucccc'
+    delete_bucket(bucket_name)
+
